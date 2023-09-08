7cf3eb7b52fb3b80f251002cc7999cbb7ed2cccf
ALOK9442 <alok2110036@akgec.ac.in>
2023-09-09 03:04:40 +0530

    styling done

---
+++ index.js
@@ -0,0 +1,8 @@
+let myLinks = []
+
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const tabbtn = document.getElementById("tab-btn")
+const deletebtn = document.getElementById("delete-btn")
